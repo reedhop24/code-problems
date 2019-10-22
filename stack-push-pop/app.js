6ea2df73797ee65bdbbe4b324659fc0a0799da46
reedhop24 <40578449+reedhop24@users.noreply.github.com>
2019-10-22 08:08:36 -0600

    Update app.js

--- stack-push-pop/app.js
+++ stack-push-pop/app.js
@@ -1,36 +1,49 @@
-class Stack {
-    constructor() {
-      this.stack = []
+'use-strict'
+
+class Stack{
+  constructor(){
+    this.stack = [];
+  }
+
+  push(element){
+    this.stack.push(element);
+  }
+
+  pop(){
+    if(this.isEmpty() === true) {
+      return 'The stack is empty';
     }
-    
-    // Inserts the element into the top of the stack
-    push(element) {
-      this.stack.push(element)
-    }
-    
-    // Removes the element from the top of the stack and return that element
-    pop() {
-      if (this.isEmpty() === true) {
-        return 'Stack is empty!' ;
-      } else {
-        return this.stack.pop();
-      }
-    }
-    
-    // helper method
-     isEmpty() {
-        return !this.queue.length
+    else{
+      this.stack.pop();
     }
   }
 
- var newStack = new Stack;
+  isEmpty(){
+    if(this.stack.length === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
 
- newStack = [];
- console.log(newStack.isEmpty());
- newStack.push(69);
- console.log(newStack);
- newStack.pop();
- console.log(newStack);
+
+var newStack = new Stack;
+newStack.stack = [1,2,3,4,5];
+
+// Testing Stack Push
+newStack.push(6);
+console.log(newStack.stack);
+
+//Testing Stack Pop
+newStack.pop();
+console.log(newStack.stack);
+
+// Testing Stack Pop With Empty Array
+newStack.stack = [];
+console.log(newStack.pop());
+ 
 
  
 
