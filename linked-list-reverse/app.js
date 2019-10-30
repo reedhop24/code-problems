429c4e07dde14baaee2879c596c7f9cedc41e96c
Reed Hopkins <reed@ibqsystems.com>
2019-10-30 08:06:50 -0600

    Commiting reversed linked list

---
+++ linked-list-reverse/app.js
@@ -0,0 +1,60 @@
+'use-strict'
+
+class LinkedList {
+    constructor(value){
+        this.head = null;
+        this.length = 0;
+        this.addToHead(value);
+    }
+
+    addToHead(value){
+        var newNode = {value};
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        regular.unshift(newNode.value);
+        return this;
+    }
+
+    removeHead(){
+        if(this.length === 0){
+            return undefined;
+        }
+        const value = this.head;
+        this.head = this.head.next;
+        this.length--;
+        return value;
+    }
+
+    reverseLinkedList(){
+        if(!this.head || !this.head.next){
+            console.log('List is empty');
+            return;
+        }
+        var p1 = null;
+        var p2 = this.head;
+        var p3;
+        while(p2){
+            p3 = p2.next;
+            p2.next = p1;
+            p1 = p2;
+            p2 = p3;
+        }
+        this.head = p1;
+        return this;
+    }
+}
+
+
+const list = new LinkedList('d')
+    .addToHead('e')
+    .addToHead('e')
+    .addToHead('r');
+    list.reverseLinkedList();
+    console.log(list);
+
+
+
+
+    
+    
