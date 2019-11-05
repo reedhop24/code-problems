a489b24ac587f424d0c651016196ab7971b14eb3
Reed Hopkins <reed@ibqsystems.com>
2019-11-05 08:13:41 -0700

    Commiting BinarySort

---
+++ binarySearch/app.js
@@ -0,0 +1,27 @@
+'use-strict';
+
+var binarySort = (arr, target) => {
+    arr = arr.sort();
+    let left = 0;
+    let right = arr.length - 1;
+    while(left <= right){
+        const mid = left + Math.floor((right - left)/2);
+        if(arr[mid] === target){
+            return mid;
+        }
+        if(arr[mid] < target){
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+// Should return 1
+console.log(binarySort([1,2,3,4,5,6], 2));
+
+// Unsorted should return 6
+console.log(binarySort([3,5,8,1,2,4,6], 8));
+
+
