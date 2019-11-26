27d91320909f9f72a78a4dd13b9d2a4e3ef1d8a3
Reed Hopkins <reed@ibqsystems.com>
2019-11-25 17:17:16 -0700

    Commiting hash map with tests

--- linked-list-nth/app.js
+++ linked-list-nth/app.js
@@ -1,53 +1,50 @@
-'use-strict'
+'use strict';
 
 class LinkedList {
 
-    constructor(value){
-        this.head = null;
-        this.length = 0;
-        this.addToHead(value);
-    }
+  constructor(value){
+    this.head = null;
+    this.length = 0;
+    this.addToHead(value);
+  }
 
-    addToHead(value){
-        var newNode = {value};
-        newNode.next = this.head;
-        this.head = newNode;
-        this.length++;
-        return this;
-    }
+  addToHead(value){
+    var newNode = {value,};
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 
-    removeHead(){
-        if(this.length === 0){
-            return undefined;
-        }
-        const value = this.head;
-        this.head = this.head.next;
-        this.length--;
-        return value;
+  removeHead(){
+    if(this.length === 0){
+      return undefined;
     }
+    const value = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return value;
+  }
 
-    findNth(value){
-        var currentNode = this.head;
-        var len = this.length;
-        while(currentNode){
-            if(len === value){
-                return currentNode;
-            }
-            len--;
-            currentNode = currentNode.next;
-        }
-        return currentNode;
+  findNth(value){
+    var currentNode = this.head;
+    var len = this.length;
+    while(len !== value){
+      len--;
+      currentNode = currentNode.next;
     }
+    return currentNode;
+  }
 }
 
 
 const list = new LinkedList('first')
-    .addToHead('second')
-    .addToHead('third')
-    .addToHead('fourth');
-    
-    console.log(list);
-    console.log(list.findNth(1));
-    console.log(list.findNth(2));
-    console.log(list.findNth(3));
-    console.log(list.findNth(4));
+  .addToHead('second')
+  .addToHead('third')
+  .addToHead('fourth');
+
+console.log(list);
+console.log(list.findNth(1));
+console.log(list.findNth(2));
+console.log(list.findNth(3));
+console.log(list.findNth(4));
